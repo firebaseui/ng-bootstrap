6a0feb0e1ea50625755d01fed956e75a704d2442
Anthony Nahas <anthony.na@hotmail.de>
2018-11-02 15:26:04 +0100

    feat(demo): support of prerendering

--- demo/src/app/home/home.component.spec.ts
+++ demo/src/app/home/home.component.spec.ts
@@ -1,7 +1,7 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {HomeComponent} from './home.component';
-import {LibModule} from 'ng-bootstrap-auth-firebaseui';
+import {NgbAuthFirebaseUIModule} from '@firebaseui/ng-bootstrap';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -10,7 +10,7 @@ describe('HomeComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        LibModule.forRoot(),],
+        NgbAuthFirebaseUIModule.forRoot(null)],
       declarations: [HomeComponent]
     })
       .compileComponents()
